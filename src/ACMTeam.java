20e3ce02b16e1c8ad31521a07086c444af7569fd
Ranada Proshad Biswas <proshad.biswas@tu.no>
2017-01-18 17:15:48 +0600

    ACM PROBLEM SOLUTION WITH BIGINTEGER

--- src/ACMTeam.java
+++ src/ACMTeam.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -5,21 +6,33 @@ import java.util.Scanner;
  */
 public class ACMTeam {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int people = in.nextInt();
-        int topics = in.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int numOfPeople = scanner.nextInt();
+        int numOfTopics = scanner.nextInt();
+        BigInteger[] visitTopics = new BigInteger[numOfPeople];
+        int maxTopicsCount = 0;
+        int maxTeamCount = 0;
+        BigInteger combine;
 
-        int[][] a = new int[people][topics];
-        for(int i=0; i < people; i++){
-            String visitTopics = in.next();
-            int[] currentTopics = new int[topics];
-            for(int j=0; j<visitTopics.length();j++){
-                currentTopics[j]=Integer.parseInt(visitTopics.charAt(j)+"");
-            }
-            a[i] = currentTopics;
+
+        for(int i=0; i<numOfPeople; i++) {
+            visitTopics[i] = scanner.nextBigInteger(2);
         }
+        // simply do a binary or of each combination of binary  e.g  100 | 001 == 101  then count the no of 1's
+        for(int i=0; i<visitTopics.length-1; i++) {
+            for(int j=i+1; j<=visitTopics.length-1; j++) {
+                combine = visitTopics[i].or(visitTopics[j]);
+                int bitCount = combine.bitCount();
+                if(bitCount > maxTopicsCount) {
+                    maxTopicsCount = bitCount;
+                    maxTeamCount = 1;
+                } else if(bitCount == maxTopicsCount) {
+                    maxTeamCount++;
+                }
+            }
+        }
+        System.out.println(maxTopicsCount);
+        System.out.println(maxTeamCount);
 
-        int totalTeam =0;
-        int totalSolve = 0;
     }
 }
