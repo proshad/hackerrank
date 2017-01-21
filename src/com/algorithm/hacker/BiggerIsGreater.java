65025a0ba61d8dc5661fb06db3b8642e840817b5
Proshad <proshad@cefalo.com>
2017-01-21 14:29:22 +0600

    add package structure

---
+++ src/com/algorithm/hacker/BiggerIsGreater.java
@@ -0,0 +1,11 @@
+package com.algorithm.hacker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Proshad
+ * Date: 1/21/17
+ * Time: 2:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BiggerIsGreater {
+}
