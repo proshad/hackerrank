1770654552f2d28bdd5a72c83320995974cb7059
Ranada Proshad Biswas <proshad.biswas@tu.no>
2017-02-15 11:11:55 +0600

    solve running median without heap

--- src/com/hacker/datastructure/heap/RunningMedian.java
+++ src/com/hacker/datastructure/heap/RunningMedian.java
@@ -9,34 +9,43 @@ public class RunningMedian {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        LinkedList<Integer> list = new LinkedList<Integer>();
+        int[] list = new int[n];
         double median = 0.0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             int val = sc.nextInt();
-
-            // sort the list
-            if (list.size() == 0) {
-                list.add(val);
-            } else if (list.get(0) > val) {
-                list.add(0, val);
-            } else if (list.get(list.size() - 1) < val) {
-                list.add(list.size(), val);
-            } else {
-                int j = 0;
-                while (list.get(j) < val) {
-                    j++;
+            if (i == 0) {
+                list[i] = val;
+                median = val;
+            } else if (i == 1) {
+                if (val > list[0]) {
+                    list[i] = val;
+                } else {
+                    int temp = list[0];
+                    list[0] = val;
+                    list[i] = temp;
                 }
-                list.add(j, val);
-            }
-
-            if(list.size()%2==0){
-                int mid = list.size()/2;
-                median = (double)(list.get(mid - 1) + list.get(mid))/2;
-            }else {
-                median = list.get(list.size()/2);
+                median = (double) (list[0] + list[1]) / 2;
+            } else {
+                sortArray(list, val, i);
+                if (i % 2 != 0) {
+                    int mid = i / 2;
+                    median = (double) (list[mid] + list[mid + 1]) / 2;
+                } else {
+                    median = list[i / 2];
+                }
             }
             System.out.println(median);
-
         }
+    }
+
+    private static int[] sortArray(int[] arr, int val, int last) {
+        int i = last;
+        while ((i > 0) && (val < arr[i-1]))
+        {
+            arr[i] = arr[i-1];
+            i = i - 1;
+        }
+        arr[i] = val;
+        return arr;
     }
 }
